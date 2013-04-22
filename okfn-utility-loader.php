7b65f631e7773ffa4083b97e5281c7015745857c
Bobby Donovan <bobby.donovan@gmail.com>
2013-04-22 12:16:00 -0400

    Initial commit with basic functions for cookie bar manipulation

---
+++ okfn-utility-loader.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: OKFN Utility
+Plugin URI: http://okfn.org
+Description: Utility functions for OKFN Blogfarm
+Version: 1.0
+Author: Bobby Donovan
+Author URI: http://bobbydonovan.com
+Author Email: bobby@bobbydonovan.com
+License:
+
+  Copyright 2013 TODO (email@domain.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+$okfn_utility_inc_dir = ( WPMU_PLUGIN_DIR == dirname(__FILE__) ) ? WPMU_PLUGIN_DIR . '/okfn-utility' : dirname(__FILE__);
+require_once( $okfn_utility_inc_dir . '/okfn-utility.php' );
+$GLOBALS['okfn_utility'] = new OKFN_Utility();
+$GLOBALS['okfn_utility']->init();
---
+++ okfn-utility.php
@@ -0,0 +1,58 @@
+<?php
+
+class OKFN_Utility {
+    
+    const SLUG = 'okfn-utility';
+	
+    public static function init() {
+        
+        // Load plugin text domain
+        add_action( 'init', array( get_class(), 'plugin_textdomain' ) );
+
+        // Transparency for Cookie notification bar
+        add_action( 'init', array( get_class(), 'cookie_notification_styles' ) );
+        add_action( 'init', array( get_class(), 'force_cookie_policy_page_creation' ) );
+        
+    } // end init
+	
+
+	/**
+	 * Loads the plugin text domain for translation
+	 */
+	public function plugin_textdomain() {
+            
+            $locale = apply_filters( 'plugin_locale', get_locale(), self::SLUG );
+            load_textdomain( self::SLUG, WP_LANG_DIR.'/'.self::SLUG.'/'.self::SLUG.'-'.$locale.'.mo' );
+            load_plugin_textdomain( self::SLUG, FALSE, plugin_dir_url( __FILE__ ) . 'lang/' );
+
+	} // end plugin_textdomain
+	
+	
+	/*--------------------------------------------*
+	 * Core Functions
+	 *---------------------------------------------*/
+	
+        function cookie_notification_styles() {
+            if ( function_exists( catapult_add_cookie_bar ) ) {
+                wp_enqueue_style( 'okf-transparent-cookie-bar', plugin_dir_url( __FILE__ ) . 'css/cookie-bar.css', false, false);
+            }
+        }
+        
+        // Currently the http://wordpress.org/extend/plugins/uk-cookie-consent/ plugin only creates the policy page when you view the plugin settings page itself. 
+        // As I don't want to do this on every site, this function will check for policy page existance and add it if necessary. 
+        // This would be better handled on plugin activation - http://wordpress.org/support/topic/suggestion-create-policy-page-on-plugin-activation?replies=1#post-4117511
+        function force_cookie_policy_page_creation() {
+            if ( function_exists('catapult_cookie_options_page') 
+                    && ! get_page_by_title( __( 'Cookie Policy', 'uk-cookie-consent' ) ) ) {
+                
+                include ABSPATH . '/wp-admin/includes/plugin.php';
+                include ABSPATH . '/wp-admin/includes/template.php';
+                ob_get_clean();
+                ob_start();
+                catapult_cookie_options_page();
+                ob_end_clean();
+            }
+        }
+        
+  
+} // end class
