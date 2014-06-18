e9055182b29f5d8fde97c0a5cf6f7d6639476d45
nigelb <nigelbabu@gmail.com>
2014-06-18 15:26:38 +0530

    Push limits to 500

--- inactive-users-list.class.php
+++ inactive-users-list.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 if(!class_exists('WP_List_Table'))
     require_once ABSPATH . '/wp-admin/includes/class-wp-list-table.php';
@@ -13,8 +13,8 @@ class OKF_Inactive_Users_List extends WP_List_Table {
             'ajax'          => false
         ) );
     }
-    
-    
+
+
     /**
     * Define the columns that are going to be used in the table
     * @return array $columns, the array of columns to use with the table
@@ -28,23 +28,23 @@ class OKF_Inactive_Users_List extends WP_List_Table {
             'col_reg_date'      => __('Registration Date')
         );
     }
-    
+
     /**
     * Prepare the table with different parameters, pagination, columns and table elements
     */
     function prepare_items() {
         global $wpdb, $_wp_column_headers;
         $screen = get_current_screen();
-        
+
         $this->process_bulk_action();
 
         $GLOBALS['okf_inactive_users']->build_user_list_array();
-        $perpage = 100;
-        
+        $perpage = 500;
+
         //Which page is this?
         $paged = !empty($_GET["paged"]) ? mysql_real_escape_string($_GET["paged"]) : '';
         if(empty($paged) || !is_numeric($paged) || $paged<=0 ){ $paged=1; }
-        
+
         $totalpages = ceil( $GLOBALS['okf_inactive_users']->count_user_list() / $perpage );
         if( !empty($paged) && !empty($perpage) ){
             $offset = ( $paged-1 ) * $perpage;
@@ -65,29 +65,29 @@ class OKF_Inactive_Users_List extends WP_List_Table {
 
         // /* -- Fetch the items -- */
         $this->items = $GLOBALS['okf_inactive_users']->filter_user_list( (int)$perpage, (int)$offset);
-        
+
     }
-    
+
     function get_bulk_actions() {
         return $actions = array(
             'delete-all' => __( 'Delete All' ),
             'delete-selected' => __( 'Delete Selected' )
         );
     }
-    
-    function process_bulk_action() {        
-        
+
+    function process_bulk_action() {
+
         switch($this->current_action()) {
             case "delete-all":
                 $GLOBALS['okf_inactive_users']->delete_all();
                 break;
-            
+
             case "delete-selected":
                 $GLOBALS['okf_inactive_users']->delete_users($_POST[$this->_args['singular']]);
                 break;
         }
     }
-    
+
     function column_cb($item){
         return sprintf(
             '<input type="checkbox" name="%1$s[]" value="%2$s" />',
@@ -95,28 +95,28 @@ class OKF_Inactive_Users_List extends WP_List_Table {
             $item
         );
     }
-   
-    
+
+
     function column_default($item, $column_name) {
         $user = get_userdata( $item );
         switch($column_name) {
-            case "col_user_id":	
-                echo stripslashes( $item );	
+            case "col_user_id":
+                echo stripslashes( $item );
                 break;
 
-            case "col_user_login": 
-                echo stripslashes( $user->user_login ); 
+            case "col_user_login":
+                echo stripslashes( $user->user_login );
                 break;
 
-            case "col_user_email": 
-                echo stripslashes( $user->user_email ); 
+            case "col_user_email":
+                echo stripslashes( $user->user_email );
                 break;
 
-            case "col_reg_date": 
-                echo stripslashes( $user->user_registered ); 
+            case "col_reg_date":
+                echo stripslashes( $user->user_registered );
                 break;
         }
     }
-    
+
 
 }
